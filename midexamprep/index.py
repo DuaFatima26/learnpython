fbd395689c2ae82c842be8aad583bf1d08e855e2
duafatima26 <duaaghanchi10@gmail.com>
2026-04-29 11:42:00 +0500

    learn python

---
+++ midexamprep/index.py
@@ -0,0 +1,38 @@
+# list
+# names = ['Shahveer', 'Shahmeer', 'Yasmeen']
+# names[2] = "Hannah" 
+# print ("Orignal names list \n" , names )  
+# to add an element at the last index or to expand list
+# names.append("Duaa") 
+# ['Shahveer', 'Shahmeer', 'Yasmeen', 'Duaa'] output
+# names.insert( 4 , "Atif")
+# yahan yeh sahi method haii jo index exist nhii krta ths phelay usnay woh banaya phir value add ki 
+# names.insert and apppend works same
+# motorcycles[4] = "Jane"  THIS IS WRONG BECAUSE 4TH INDEX DOESNT EXIST 
+# to delete a value in list there are three ways pop delete remove 
+# names.remove("Duaa")
+# del names[0]
+# word = names.pop()
+# # word = atif
+# print (names)
+# print (word)
+# corrrect
+
+# Numbers = [ 1 , 79 , 56 ,90 , 100 , 1 , 0 , 45 ,34, 343567, 67, 26 , 28 ]
+# Numbers.sort()
+# Numbers.sort(reverse = True)
+# print("Orifnal list of number\n " , Numbers , "\n" "This permenantly sorts the list sort() \n" , Numbers)
+# print( "This sort the list in reverse order : number.sort(reverse = true) \n" , Numbers)
+
+# Value = [44 , 66 , 767, 89 , 3, 12 ,0 , -1, -5, -0, -556, ]
+# print("This temporarily sorts the list : \n" , sorted(Value))
+# print( "orginal values list" , Value)
+
+# Names = ["Duaa" , "Jnae" ,  "Sahil"]
+# for Name in Names:
+#    print (f"my name is {Name}")
+
+Names = [ 1 , "Duaa" , "Jane" , [2 , "yasmeen" , "sharif"], [24 , 78 , 9]]
+
+print(Names)
+print(Names[:])
